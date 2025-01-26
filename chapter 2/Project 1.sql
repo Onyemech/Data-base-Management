636cfa1118eda2ec9fbe0c14ed7b9a199b01be4d
Onyemechi Caleb <Onyemechicaleb4@gmail.com>
2025-01-26 22:17:38 +0100

    A - E

---
+++ chapter 2/Project 1.sql
@@ -0,0 +1,29 @@
+USE MDC_CH02_CQ;
+
+-- Task A
+-- SELECT * FROM CUSTOMER;
+-- SELECT * FROM INVOICE;
+-- SELECT * FROM INVOICE_ITEM;
+
+-- TASK B
+-- SELECT LastName, FirstName,Phone
+-- -- concat(lastName,' ',firstName,' ',phone) 
+-- FROM mdc_ch02_cq.customer
+
+-- TASK C
+-- SELECT LastName, FirstName,Phone
+-- FROM mdc_ch02_cq.customer
+-- WHERE FirstName = 'Nikki'; 
+
+-- TASK D
+-- SELECT c.LastName, c.FirstName, c.Phone, i.DateIn, i.DateOut
+-- FROM mdc_ch02_cq.customer c
+-- JOIN mdc_ch02_cq.invoice i
+-- WHERE TotalAmount > 100.00;
+
+-- TASK E
+SELECT LastName,FirstName,Phone
+FROM mdc_ch02_cq.customer
+WHERE FirstName LIKE 'B%';
+
+
